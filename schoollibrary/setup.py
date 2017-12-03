680de3bc23a9a1277fdf38974053c6b7d31d2707
Youssef <youssef@localhost.localdomain>
2017-12-03 18:54:08 +0000

    add static files

--- schoollibrary/setup.py
+++ schoollibrary/setup.py
@@ -6,5 +6,15 @@ setup(
     include_package_data=True,
     install_requires=[
         'flask',
+        'flask_bcrypt',
+        'flask_login',
+        'flask_wtf',
+        'wtforms',
+        'sqlalchemy',
+        'numpy',
+        'pillow',
+        'zbar_py',
+        'pyisbn',
+        'uwsgi'
     ],
 )
