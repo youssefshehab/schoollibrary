3244e67c038402495664cd37c4766a92c6d5cee3
yshehab@github.com <yshehab@github.com>
2018-10-08 23:57:39 +0100

    update copy right year.

--- schoollibrary/bpslibrary/__init__.py
+++ schoollibrary/bpslibrary/__init__.py
@@ -7,7 +7,7 @@
     
     :copyright:
     
-    Copyright (C) 2017 Youssef Shehab
+    Copyright (C) 2018 Youssef Shehab
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
